4ea20d745c0372e67ae898b200258fb6d43ee31d
GuidoPerren <andringuido.perren@my.jcu.edu.au>
2023-03-20 11:09:34 +0800

    refactored score.py and added Part 2

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,16 +2,41 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
 
-# TODO: Fix this!
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-else:
-    if score >= 90:
-        print("Excellent")
-    elif score >= 50:
-        print("Passable")
-    elif score < 50:
-        print("Bad")
+def grade_score(score):
+
+    grade_text = ""
+
+    if score < 0 or score > 100:
+        grade_text = ("Invalid score")
+    else:
+        if score >= 90:
+            grade_text = ("Excellent")
+        elif score >= 50:
+            grade_text = ("Passable")
+        elif score < 50:
+            grade_text = ("Bad")
+
+    return  grade_text
+
+
+
+
+def main():
+    """
+    main program
+    """
+
+    #Part 1
+    #score = float(input("Enter score: "))
+    #grade_text = grade_score(score)
+    #print(grade_text)
+
+    #Part 2
+    random_score = random.randrange(0, 101, 1)
+    grade_text = grade_score(random_score)
+    print(f"The score {random_score} is an {grade_text} grade")
+
+main()
