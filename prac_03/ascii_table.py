b660cc783583a6c611d77048c9ccb3a7fe1c8dc0
GuidoPerren <andringuido.perren@my.jcu.edu.au>
2023-03-27 11:19:52 +0800

    created ascii_table.py and updated files.py

---
+++ prac_03/ascii_table.py
@@ -0,0 +1,14 @@
+MIN_NUMBER = 33
+MAX_NUMBER = 127
+
+char_input = str(input("Enter a character: "))
+print(f"The ASCII code for {char_input} is {ord(char_input)}")
+
+try:
+    number_input = int(input(f"Enter a number between {MIN_NUMBER} and {MAX_NUMBER}: "))
+    if(MIN_NUMBER <= number_input <= MAX_NUMBER):
+        print(f"The character for {number_input} is {chr(number_input)}")
+    else:
+        print("Number is outside the range")
+except ValueError:
+    print("Please Enter a valid Integer")
--- prac_03/files.py
+++ prac_03/files.py
@@ -18,25 +18,28 @@ def get_text_file_content(path):
 FILE_NAME_EXCERSISE_1 = "names.txt"
 FILE_NAME_EXCERSISE_2 = "numbers.txt"
 
-# Write to File
-name_of_user = str(input("Enter your name: "))
-write_text_file(FILE_NAME_EXCERSISE_1, name_of_user)
+def main():
+    # Write to File
+    name_of_user = str(input("Enter your name: "))
+    write_text_file(FILE_NAME_EXCERSISE_1, name_of_user)
 
-# Read "name.txt" File
-lines_read = get_text_file_content(FILE_NAME_EXCERSISE_1)
-print(f"Your name is {lines_read[0]}")  # the name is stored in the first line, hence [0]
+    # Read "name.txt" File
+    lines_read = get_text_file_content(FILE_NAME_EXCERSISE_1)
+    print(f"Your name is {lines_read[0]}")  # the name is stored in the first line, hence [0]
 
 
-# Read "number.txt" File
-lines_read = get_text_file_content(FILE_NAME_EXCERSISE_2) #should override the existing lines_read var
-line_1 = int(lines_read[0])
-line_2 = int(lines_read[1])
-result = line_1 + line_2
-print(str(result))
+    # Read "number.txt" File
+    lines_read = get_text_file_content(FILE_NAME_EXCERSISE_2) #should override the existing lines_read var
+    line_1 = int(lines_read[0])
+    line_2 = int(lines_read[1])
+    result = line_1 + line_2
+    print(str(result))
 
-#Add them all up
-result = 0
-for line in lines_read:
-    result += int(line)
+    #Add them all up
+    result = 0
+    for line in lines_read:
+        result += int(line)
 
-print(result)
+    print(result)
+
+main()
