832797115c0b00e8968d8248afec11262b266fbb
GuidoPerren <andringuido.perren@my.jcu.edu.au>
2023-03-27 11:24:12 +0800

    added a function to print the whole table

--- prac_03/ascii_table.py
+++ prac_03/ascii_table.py
@@ -1,14 +1,20 @@
 MIN_NUMBER = 33
 MAX_NUMBER = 127
 
-char_input = str(input("Enter a character: "))
-print(f"The ASCII code for {char_input} is {ord(char_input)}")
+def main():
+    char_input = str(input("Enter a character: "))
+    print(f"The ASCII code for {char_input} is {ord(char_input)}")
 
-try:
-    number_input = int(input(f"Enter a number between {MIN_NUMBER} and {MAX_NUMBER}: "))
-    if(MIN_NUMBER <= number_input <= MAX_NUMBER):
-        print(f"The character for {number_input} is {chr(number_input)}")
-    else:
-        print("Number is outside the range")
-except ValueError:
-    print("Please Enter a valid Integer")
+    try:
+        number_input = int(input(f"Enter a number between {MIN_NUMBER} and {MAX_NUMBER}: "))
+        if(MIN_NUMBER <= number_input <= MAX_NUMBER):
+            print(f"The character for {number_input} is {chr(number_input)}")
+        else:
+            print("Number is outside the range")
+    except ValueError:
+        print("Please Enter a valid Integer")
+
+    for i in range(MIN_NUMBER, (MAX_NUMBER + 1)):
+        print(f"{i} {chr(i)}")
+
+main()
