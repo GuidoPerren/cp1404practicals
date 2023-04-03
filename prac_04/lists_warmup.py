55bddb6e638042a71bc7e1ab9d02ac752aa9d9dc
GuidoPerren <andringuido.perren@my.jcu.edu.au>
2023-04-03 09:16:45 +0800

    Added statements

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -30,3 +30,8 @@ numbers + [6, 5, 3]
 A: This will add the numbers 6, 5 and 3 to the list and print it, since there is no equal sign it will
 not override the existing numbers variable, resulting in the output = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
 """
+
+numbers[0] = "ten"
+numbers[-1] = 1
+print(numbers[2:])
+print((9 in numbers))
