8f1ac5e62800cd986c6a4a645ce39565f72c9700
GuidoPerren <andringuido.perren@my.jcu.edu.au>
2023-04-03 09:52:34 +0800

    added user authentication from list; renamed usernames to username_list

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,16 +1,26 @@
 
 def main():
     number_list = []
+    username_list = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+                 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer',
+                 'bob']
 
-    for i in range(0, 5):
-        user_input = int(input("Number: "))
-        number_list.append(user_input)
+    username_input = str(input("Username: "))
+    if(username_input in username_list):
 
-    print(number_list[0])
-    print(number_list[-1])
-    print(min(number_list))
-    print(max(number_list))
-    print((sum(number_list) / len(number_list)))
+        print("Access granted")
+
+        for i in range(0, 5):
+            user_input = int(input("Number: "))
+            number_list.append(user_input)
+
+        print(number_list[0])
+        print(number_list[-1])
+        print(min(number_list))
+        print(max(number_list))
+        print((sum(number_list) / len(number_list)))
+    else:
+        print("Access denied")
 
 
 
