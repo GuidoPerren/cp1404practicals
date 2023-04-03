4218ca26c27d96a28ab5a5eb981c41ccb78d776c
GuidoPerren <andringuido.perren@my.jcu.edu.au>
2023-04-03 11:24:51 +0800

    renamed functions print_formatted_random_numbers; refactored formatting

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -5,6 +5,20 @@ LARGEST_NUMBER = 45
 AMOUNT_OF_NUMBERS_PER_LINE = 6
 
 
+def main():
+    try:
+        quick_pick_amount = int(input("How many quick picks? "))
+        for i in range(0, quick_pick_amount):
+            random_number_list = generate_unique_randoms(
+                SMALLEST_NUMBER, LARGEST_NUMBER, AMOUNT_OF_NUMBERS_PER_LINE)
+            random_number_list = sorted(random_number_list)
+            print(" ".join(f"{number:2}" for number in random_number_list))
+            # print_formatted_random_numbers(random_number_list, (len(str(LARGEST_NUMBER)) + 1) )
+
+    except ValueError:
+        print("Please enter an integer")
+
+
 def generate_unique_randoms(smallest, largest, amount):
     random_numbers = []
     while len(random_numbers) != amount:
@@ -14,26 +28,13 @@ def generate_unique_randoms(smallest, largest, amount):
 
     return random_numbers
 
-def print_formated_random_numbers(random_number_list, width):
+
+def print_formatted_random_numbers(random_number_list, width):
     print_string = f""
     for number in random_number_list:
         print_string += f"{number:{width}}"
 
     print(print_string)
-
-def main():
-    try:
-        quick_pick_amount = int(input("How many quick picks? "))
-        for i in range(0, quick_pick_amount):
-            random_number_list = generate_unique_randoms(
-                SMALLEST_NUMBER, LARGEST_NUMBER, AMOUNT_OF_NUMBERS_PER_LINE)
-            random_number_list = sorted(random_number_list)
-            print(" ".join(f"{number:2}" for number in random_number_list))
-            #print_formated_random_numbers(random_number_list, (len(str(LARGEST_NUMBER)) + 1) )
-
-    except ValueError:
-        print("Please enter an integer")
-
 
 
 main()
