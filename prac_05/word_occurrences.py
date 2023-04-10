77fc71887a6d03229a9c3fee306e66ebe8c54730
GuidoPerren <andringuido.perren@my.jcu.edu.au>
2023-04-10 10:26:56 +0800

    formated output from the list of words

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -4,6 +4,8 @@ Estimate: 10 minutes
 Actual:   xx minutes
 """
 
+WIDTH_CORRECTION = 1
+
 def main():
     word_dict = {}
     sentence = (input("Enter short state: ")).lower()
@@ -13,6 +15,8 @@ def main():
         else:
             word_dict[word] = 1
 
-    print(word_dict)
+    longest_word_count = len(max(word_dict.keys(), key=len)) + WIDTH_CORRECTION
+    for word in word_dict:
+        print(f"{word:{longest_word_count}}: {word_dict[word]}")
 
 main()
