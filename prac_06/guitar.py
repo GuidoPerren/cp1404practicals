364b10392201a68ad681cd7b8a128155281809c7
GuidoPerren <andringuido.perren@my.jcu.edu.au>
2023-04-24 09:57:46 +0800

    switched output to enumeration

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -25,7 +25,5 @@ guitar_list.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
 guitar_list.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 guitar_list.append(Guitar("Fancy Pants", 1965, 1700.40))
 
-i = 1
-for guitar in guitar_list:
+for i, guitar in enumerate(guitar_list, 1):
     print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{guitar.vintage_string}")
-    i += 1
