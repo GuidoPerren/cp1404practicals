a2975d27ff3c2bc4c1296659c6066fdda624b05e
GuidoPerren <andringuido.perren@my.jcu.edu.au>
2023-04-24 09:32:37 +0800

    created a list with the 3 programming languages

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,10 +1,19 @@
+"""
+Expected: 1h
+Was: 50min
+"""
+
 from programming_language import ProgrammingLanguage
 
 def main():
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+
     print(python)
+
+    programming_languages = [python, ruby, visual_basic]
 
 
 main()
