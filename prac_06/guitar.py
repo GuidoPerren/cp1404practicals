95d88ddf64a384cd676168c25a9de7c24f4d7e7c
GuidoPerren <andringuido.perren@my.jcu.edu.au>
2023-04-24 10:00:10 +0800

    seperated guitar.py into guitar.py and guitar_test.py

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -16,16 +16,3 @@ class Guitar():
     def __str__(self):
         return f"{self.name} ({self.year}), worth ${self.cost}{self.vintage_string}"
 
-def main():
-    # test_guitar = Guitar(name="Fancy Pants", year=1965, cost=1700.40)
-    # print(test_guitar)
-
-    guitar_list = []
-    guitar_list.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-    guitar_list.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
-    guitar_list.append(Guitar("Fancy Pants", 1965, 1700.40))
-
-    for i, guitar in enumerate(guitar_list, 1):
-        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{guitar.vintage_string}")
-
-main()
