7872b8249a7fac3c4bc1780a88886a0eebd86a83
GuidoPerren <andringuido.perren@my.jcu.edu.au>
2023-04-24 09:56:44 +0800

    add output and to a toString method

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -4,6 +4,7 @@ Was: 1.25h
 """
 import datetime
 
+
 class Guitar():
 
     def __init__(self, name, year, cost):
@@ -15,5 +16,16 @@ class Guitar():
     def __str__(self):
         return f"{self.name} ({self.year}), worth ${self.cost}{self.vintage_string}"
 
-test_guitar = Guitar(name="Fancy Pants", year=1965, cost=1700.40)
-print(test_guitar)
+
+# test_guitar = Guitar(name="Fancy Pants", year=1965, cost=1700.40)
+# print(test_guitar)
+
+guitar_list = []
+guitar_list.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+guitar_list.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+guitar_list.append(Guitar("Fancy Pants", 1965, 1700.40))
+
+i = 1
+for guitar in guitar_list:
+    print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{guitar.vintage_string}")
+    i += 1
