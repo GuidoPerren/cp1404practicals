3bd1a66d41f924dd42bcfd773e276c926bf57a0b
GuidoPerren <andringuido.perren@my.jcu.edu.au>
2023-05-07 22:17:24 +0800

    added user prompt loop and save at the end

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -1,7 +1,10 @@
+from guitar import Guitar
+
+PATH_TO_CSV = "guitars.csv"
 def main():
     # Read the guitars from the file and store them in a list
     guitars = []
-    with open('guitars.csv', 'r') as file:
+    with open(PATH_TO_CSV, 'r') as file:
         for line in file:
             name, year, cost = line.strip().split(',')
             guitar = Guitar(name, year, cost)
@@ -17,14 +20,24 @@ def main():
     for guitar in guitars:
         print(guitar.name, guitar.year, guitar.cost)
 
-class Guitar:
-    def __init__(self, name, year, cost):
-        self.name = name
-        self.year = int(year)
-        self.cost = float(cost)
+    continue_to_run = True
+    while continue_to_run:
+        name = str(input("Name: "))
+        if name:
+            try:
+                year = int(input("Year: "))
+                cost = float(input("Cost: "))
+                guitars.append(Guitar(name, year, cost))
+                print(f"{name} ({year}) : ${cost} added.")
+                print()
 
-    def __lt__(self, other):
-        return self.year < other.year
+            except ValueError:
+                print("The values year and cost need to be numerical")
+                print()
+        else:
+            continue_to_run = False
+            with open(PATH_TO_CSV, 'w') as file:
+                for guitar in guitars:
+                    file.write(f"{guitar.name},{guitar.year},{guitar.cost}\n")
 
-if __name__ == '__main__':
-    main()
+main()
